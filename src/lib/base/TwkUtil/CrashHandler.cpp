7343120b9e9dfc1d48841c3366317ca3001c952f
Bernard Laberge <bernard.laberge@autodesk.com>
2026-04-30 11:06:10 -0400

    Try fixing linux crash on launch

--- src/lib/base/TwkUtil/CrashHandler.cpp
+++ src/lib/base/TwkUtil/CrashHandler.cpp
@@ -383,7 +383,7 @@ namespace TwkUtil
                                               "",                              // http_proxy (empty - no uploads)
                                               m_annotations,                   // annotations
                                               m_arguments,                     // arguments
-                                              false,                           // restartable: false prevents respawn loops on Linux
+                                              true,                            // restartable
                                               false);                          // asynchronous_start
 
         if (!success)
@@ -451,7 +451,7 @@ namespace TwkUtil
                                               "",                              // http_proxy
                                               m_annotations,                   // annotations
                                               m_arguments,                     // arguments
-                                              false,                           // restartable: false prevents respawn loops on Linux
+                                              true,                            // restartable
                                               false,                           // asynchronous_start
                                               m_attachments);                  // attachments
         if (!success)
